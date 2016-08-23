0ce5d897257d7b904c4c03f788440582166ffa70
Loh Fuxing <lohfuxing@gmail.com>
2016-08-23 22:44:42 +0800

    Added data source for 0.3

---
+++ src/main/java/com/munch/core/essential/source/DataSource.java
@@ -0,0 +1,25 @@
+package com.munch.core.essential.source;
+
+/**
+ * Created By: Fuxing Loh
+ * Date: 23/8/2016
+ * Time: 10:42 PM
+ * Project: essential
+ */
+public class DataSource {
+
+    // Your Bloody Horny Friend
+    public static final int Y = 2;
+    public static final int B = 3;
+    public static final int H = 1;
+    public static final int F = 4;
+
+    public static final int FACEBOOK = 5;
+    public static final int GOOGLE = 6;
+
+    public static final int WEBSITE = 100;
+    public static final int INSTAGRAM = 500;
+    public static final int BLOG = 800;
+
+    public static final int OTHER = 1000;
+}
