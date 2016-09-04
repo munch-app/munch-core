3050bfabe74bd4ff483769e774127d232f54c496
Loh Fuxing <lohfuxing@gmail.com>
2016-09-04 15:54:53 +0800

    Updated DataSource for more inclusion of data Added better build.gradle file

--- src/main/java/com/munch/core/essential/source/DataSource.java
+++ src/main/java/com/munch/core/essential/source/DataSource.java
@@ -16,7 +16,6 @@ public class DataSource {
 
     public static final int FACEBOOK = 5;
     public static final int GOOGLE = 6;
-    public static final int FACT = 10;
 
     public static final int WEBSITE = 100;
     public static final int INSTAGRAM = 500;
@@ -24,4 +23,8 @@ public class DataSource {
 
     public static final int OTHER = 1000;
     public static final int RESTAURANT = 2000;
+
+    public static final int FACT = 10_010;
+    public static final int MUNCH_STAFF = 10_012;
+    public static final int MUNCH_USER = 10_014;
 }
