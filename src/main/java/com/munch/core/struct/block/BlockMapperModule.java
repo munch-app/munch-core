42c814c639c33743dc3b7046203d0d12a25aac7b
Loh Fuxing <lohfuxing@gmail.com>
2016-09-22 19:00:18 +0800

    Added block mapper module as a default module of struct

---
+++ src/main/java/com/munch/core/struct/block/BlockMapperModule.java
@@ -0,0 +1,34 @@
+package com.munch.core.struct.block;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.munch.core.essential.json.AwsPersistClient;
+import com.munch.core.essential.json.BlockMapper;
+import com.munch.core.essential.json.GsonConverter;
+import com.munch.core.essential.json.JsonConverter;
+import com.munch.core.struct.block.source.SeedPlace;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * Created By: Fuxing Loh
+ * Date: 22/9/2016
+ * Time: 6:58 PM
+ * Project: struct
+ */
+public class BlockMapperModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(JsonConverter.class).to(GsonConverter.class);
+    }
+
+    @Provides
+    @Named(SeedPlace.BUCKET_NAME)
+    @Singleton
+    BlockMapper provideSeedPlaceBlockMapper(JsonConverter jsonConverter) {
+        return new BlockMapper(new AwsPersistClient(SeedPlace.BUCKET_NAME), jsonConverter);
+    }
+
+}
