256720a8526654b177ab3d3015747d0193296bd9
Loh Fuxing <lohfuxing@gmail.com>
2016-09-29 18:37:27 +0800

    Removed 1 more data source

--- src/main/java/com/munch/core/essential/source/MunchSource.java
+++ src/main/java/com/munch/core/essential/source/MunchSource.java
@@ -12,13 +12,14 @@ public class MunchSource {
     public static final int GOOGLE = 6;
 
     public static final int WEBSITE = 100;
+
+    // Corpus
     public static final int INSTAGRAM = 500;
     public static final int BLOG = 800;
 
     public static final int OTHER = 1000;
     public static final int RESTAURANT = 2000;
 
-    public static final int FACT = 10_010;
     public static final int MUNCH_STAFF = 10_012;
     public static final int MUNCH_USER = 10_014;
 
