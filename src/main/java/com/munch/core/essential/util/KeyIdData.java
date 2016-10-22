a27494cc43e2ed2e36e6b42c2a431743ad1b4418
Fuxing Loh <lohfuxing@gmail.com>
2016-10-23 04:27:20 +0800

    Added key id data type for easy getting of key data

---
+++ src/main/java/com/munch/core/essential/util/KeyIdData.java
@@ -0,0 +1,13 @@
+package com.munch.core.essential.util;
+
+/**
+ * Created by: Fuxing
+ * Date: 23/10/2016
+ * Time: 4:25 AM
+ * Project: essential
+ */
+public interface KeyIdData {
+
+    String getKey();
+
+}
