92416af0f92ba0f645eab85adb8e3ba42898155f
Fuxing Loh <lohfuxing@gmail.com>
2017-07-06 06:18:00 +0800

    Added new wip: munch-search service for all search related indexing

---
+++ service-search/src/main/java/munch/search/SearchApi.java
@@ -0,0 +1,14 @@
+package munch.search;
+
+import com.google.inject.Singleton;
+import munch.restful.server.RestfulServer;
+
+/**
+ * Created by: Fuxing
+ * Date: 6/7/2017
+ * Time: 6:17 AM
+ * Project: munch-core
+ */
+@Singleton
+public class SearchApi extends RestfulServer {
+}
