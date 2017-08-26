5e11683d2b6a2e001910d85589ead48e28469bc9
Fuxing Loh <lohfuxing@gmail.com>
2017-08-27 01:01:05 +0800

    Fixed missing install ImageModule

--- catalyst-munch/src/main/java/munch/catalyst/MunchModule.java
+++ catalyst-munch/src/main/java/munch/catalyst/MunchModule.java
@@ -6,6 +6,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.multibindings.Multibinder;
+import corpus.blob.ImageModule;
 import corpus.data.DataModule;
 import munch.catalyst.clients.ClientModule;
 
@@ -22,6 +23,7 @@ public class MunchModule extends AbstractModule {
         install(new CatalystModule());
         install(new DataModule());
         install(new ClientModule());
+        install(new ImageModule());
         bind(CatalystEngine.class).to(MunchCatalyst.class);
 
         Multibinder<AbstractIngress> routerBinder = Multibinder.newSetBinder(binder(), AbstractIngress.class);
