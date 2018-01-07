be8e8d121e9e158c4cb419d88217a5e47f2fdc82
Fuxing <lohfuxing@gmail.com>
2018-01-07 21:59:15 +0800

    Added api for extended RIP content instagram & article

--- service-api/src/main/java/munch/api/services/PlaceService.java
+++ service-api/src/main/java/munch/api/services/PlaceService.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import munch.api.services.places.PlaceCardReader;
+import munch.article.clients.Article;
+import munch.article.clients.ArticleClient;
+import munch.corpus.instagram.InstagramMedia;
+import munch.corpus.instagram.InstagramMediaClient;
 import munch.data.clients.PlaceClient;
 import munch.data.structure.Place;
 import munch.data.structure.PlaceCard;
@@ -21,11 +25,15 @@ import java.util.List;
 @Singleton
 public class PlaceService extends AbstractService {
     private final PlaceClient dataClient;
+    private final ArticleClient articleClient;
+    private final InstagramMediaClient instagramMediaClient;
     private final PlaceCardReader cardReader;
 
     @Inject
-    public PlaceService(PlaceClient placeClient, PlaceCardReader cardReader) {
+    public PlaceService(PlaceClient placeClient, ArticleClient articleClient, InstagramMediaClient instagramMediaClient, PlaceCardReader cardReader) {
         this.dataClient = placeClient;
+        this.articleClient = articleClient;
+        this.instagramMediaClient = instagramMediaClient;
         this.cardReader = cardReader;
     }
 
@@ -42,8 +50,8 @@ public class PlaceService extends AbstractService {
 
             // Additional sorted data
             PATH("/data", () -> {
-                // Instagram
-                // Article
+                GET("/article", this::getArticles);
+                GET("/instagram", this::getInstagramMedias);
                 // Reviews
             });
         });
@@ -78,5 +86,24 @@ public class PlaceService extends AbstractService {
         objectNode.set("cards", objectMapper.valueToTree(cards));
         objectNode.set("place", objectMapper.valueToTree(place));
         return nodes(200, objectNode);
+    }
+
+    private List<Article> getArticles(JsonCall call) {
+        String placeId = call.pathString("placeId");
+        String maxSort = call.queryString("maxSort", null);
+        return articleClient.list(placeId, null, maxSort, querySize(call));
+    }
+
+    private List<InstagramMedia> getInstagramMedias(JsonCall call) {
+        String placeId = call.pathString("placeId");
+        String maxSort = call.queryString("maxSort", null);
+        return instagramMediaClient.listByPlace(placeId, null, maxSort, querySize(call));
+    }
+
+    private static int querySize(JsonCall call) {
+        int size = call.queryInt("size", 20);
+        if (size <= 0) return 20;
+        if (size >= 20) return 20;
+        return size;
     }
 }
