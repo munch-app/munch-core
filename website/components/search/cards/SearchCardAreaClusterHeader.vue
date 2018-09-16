71d88c0e3b95411183fca84928832df0f93dcbd1
Fuxing Loh <lohfuxing@gmail.com>
2018-09-17 02:34:09 +0800

    Added new SearchCardAreaClusterHeader

--- website/components/search/cards/SearchCardAreaClusterHeader.vue
+++ website/components/search/cards/SearchCardAreaClusterHeader.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h2>{{area.name}}</h2>
+    <div class="SearchCard Secondary400Bg Border4 ZeroSpacing">
+      <h2 class="White Text Weight600">{{area.name}}</h2>
+      <div class="Content">
+        <image-size v-if="image" class="Image Border4" :image="image"/>
+        <div class="Text White">
+          <div>{{area.description}}</div>
+          <div class="LastLine">
+            <div class="Count">
+              <span class="Weight600">{{area.counts.total}} </span>
+              <span>food spots</span>
+            </div>
+            <div class="Address">{{area.location.address}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,11 +40,40 @@
 </script>
 
 <style scoped lang="less">
-  .Image {
-    width: 100%;
-    height: 216px;
+  .SearchCard {
+    padding: 24px 24px;
+    overflow: hidden;
   }
 
-  h2 {
+  .Content {
+    margin-top: 8px;
+    display: flex;
+  }
+
+  .Image {
+    z-index: 1;
+    padding-left: 150px;
+    padding-top: 100px;
+    margin-right: 24px;
+  }
+
+  .LastLine {
+    display: flex;
+    margin-top: 8px;
+
+    .Count {
+      margin-right: 32px;
+    }
+  }
+
+  @media (max-width: 575.98px) {
+    .Content {
+      flex-direction: column;
+    }
+
+    .Image {
+      width: 100%;
+      margin-bottom: 16px;
+    }
   }
 </style>
