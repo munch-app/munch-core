4da89a713f8f522d4fdb97a90da298f81f4005b5
Fuxing Loh <lohfuxing@gmail.com>
2018-09-19 18:02:55 +0800

    Added about content

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -1,18 +1,50 @@
 <template>
-  <section class="Container About">
-    <h1>About MUNCH</h1>
-  </section>
+  <div class="About">
+    <section class="Munch Container">
+      <h2>Munch is 'the guy'</h2>
+      <p>
+        The one you call when you need the perfect dinner spot.
+        The fellow you text to ask where the absolute best chicken rice stall is to
+        settle the score with your friends. The type of foodie that can launch into
+        detail about all the places you’d never thought to try or simply point you to
+        the perfect hole-in-the-wall to satisfy your cravings.
+      </p>
+      <p>Munch is the person who you know - knows.</p>
+    </section>
+
+    <section class="Vision Container">
+      <h2>Our Vision</h2>
+      <p>Our Vision at Munch is to put food in front of you so quickly that it’ll make your Grandma jealous.</p>
+      <p>Sorry, Grandma</p>
+    </section>
+
+    <section class="Mission Container">
+      <h2>Our Mission</h2>
+      <p>Our Mission at Munch is to help anyone find the perfect place to have a hearty meal.</p>
+      <p>It doesn’t matter if you’re having dinner with your lover or your Labrador retriever, we’ll find you a place
+        so good you’d think we’ve been quietly watching you eat for years.</p>
+    </section>
+
+    <section class="Contact Container">
+      <h2>Talk to Us</h2>
+      <p>Food and conversations go well just like cheese and wine do. Feel free to contact us if you have any questions, or if you simply want to grab a meal with us.</p>
+      <p>contact@munch.space</p>
+    </section>
+  </div>
 </template>
 
 <script>
-  export default {
-
-  }
+  export default {}
 </script>
 
 <style scoped lang="less">
   .About {
     margin-top: 24px;
     padding-bottom: 64px;
+  }
+
+  section {
+    margin-top: 48px;
+    margin-bottom: 48px;
   }
 </style>
