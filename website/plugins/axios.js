3893a0dbcf37adcb7f0d0747e5e3475fcb083fe0
Fuxing Loh <lohfuxing@gmail.com>
2018-09-21 22:17:52 +0800

    User Lat Lng will be persisted for 30 minutes

--- website/plugins/axios.js
+++ website/plugins/axios.js
@@ -1,14 +1,16 @@
 import dateformat from 'dateformat'
 import authenticator from '~/services/authenticator'
+import * as Cookies from 'js-cookie'
 
 export default function ({$axios, store, req}) {
   if (process.client) {
     $axios.onRequest(config => {
       // User Data from state
       config.headers['User-Local-Time'] = dateformat(new Date(), "yyyy-mm-dd'T'HH:MM:ss")
-      const latLng = store.state.filter.user.latLng
-      if (latLng) config.headers['User-Lat-Lng'] = latLng
 
+      const latLng = store.state.filter.user.latLng || Cookies.get('UserLatLng')
+      if (latLng) config.headers['User-Lat-Lng'] = latLng
+      
       // Get user Id token
       return authenticator.getIdToken().then(({token}) => {
         if (token) config.headers['Authorization'] = `Bearer ${token}`
@@ -23,6 +25,9 @@ export default function ({$axios, store, req}) {
     $axios.onRequest(config => {
       const token = req.cookies.IdToken && JSON.parse(req.cookies.IdToken).token
       if (token) config.headers['Authorization'] = `Bearer ${token}`
+
+      const latLng = req.cookies.UserLatLng
+      if (latLng) config.headers['User-Lat-Lng'] = latLng
       return config
     });
   }
--- website/store/filter.js
+++ website/store/filter.js
@@ -1,13 +1,10 @@
+import * as Cookies from 'js-cookie'
+
 const ANYWHERE = {
-  areaId: 'singapore',
-  type: 'City',
-  name: 'Singapore',
-  images: [],
-  hour: [],
+  areaId: 'singapore', type: 'City', name: 'Singapore',
+  images: [], hour: [],
   location: {
-    city: 'Singapore',
-    country: 'SGP',
-    latLng: '1.290270, 103.851959',
+    city: 'Singapore', country: 'SGP', latLng: '1.290270, 103.851959',
     polygon: {
       points: ["1.26675774823,103.603134155", "1.32442122318,103.617553711", "1.38963424766,103.653259277", "1.41434608581,103.666305542", "1.42944763543,103.671798706", "1.43905766081,103.682785034", "1.44386265833,103.695831299", "1.45896401284,103.720550537", "1.45827758983,103.737716675", "1.44935407163,103.754196167", "1.45004049736,103.760375977", "1.47887018872,103.803634644", "1.4754381021,103.826980591", "1.45827758983,103.86680603", "1.43219336108,103.892211914", "1.4287612035,103.897018433", "1.42670190649,103.915557861", "1.43219336108,103.934783936", "1.42189687297,103.960189819", "1.42464260763,103.985595703", "1.42121043879,104.000701904", "1.43974408965,104.02130127", "1.44592193988,104.043960571", "1.42464260763,104.087219238", "1.39718511473,104.094772339", "1.35737118164,104.081039429", "1.29009788407,104.127044678", "1.277741368,104.127044678", "1.25371463932,103.982162476", "1.17545464492,103.812561035", "1.13014521522,103.736343384", "1.19055762617,103.653945923", "1.1960495989,103.565368652", "1.26675774823,103.603134155"]
     }
@@ -106,6 +103,8 @@ export const mutations = {
    */
   updateLatLng(state, latLng) {
     state.user.latLng = latLng
+    // Expire in 30 minutes
+    Cookies.set('UserLatLng', latLng, {expires: 1.0 / 48.0})
   },
 
   putTag(state, tag) {
@@ -266,6 +265,11 @@ export const actions = {
   start({commit, state}, query) {
     if (state.loading === true) return
     commit('loading', true)
+
+    // Set User LatLng if exist
+    const cookieLatLng = Cookies.get('UserLatLng')
+    if (!state.user.latLng && cookieLatLng) commit('updateLatLng', cookieLatLng)
+
     if (query) commit('replace', query)
 
     // Search Preference Injection
