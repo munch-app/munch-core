22e2823556c4d1dd9d39dd49016174c1eabe45c4
Fuxing Loh <lohfuxing@gmail.com>
2018-09-24 00:42:31 +0800

    Added next/prev horizontal scroll view control

--- website/components/core/HorizontalScrollView.vue
+++ website/components/core/HorizontalScrollView.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="HorizontalScrollView zero-spacing" :class="{container}">
-    <div class="ScrollArea" :style="container ? '' : 'margin: 0 -15px'">
-      <div class="Content" :style="{paddingLeft: `${15-padding/2}px`, paddingRight: `${15-padding/2}px`}">
-        <div class="Item" :style="{paddingLeft: `${padding/2}px`, paddingRight: `${padding/2}px`}" v-for="item in items" :key="mapKey(item)">
-          <slot :item="item">{{item}}</slot>
+  <div class="HorizontalScrollViewWrapper" :class="{'container-no-gutter': container}">
+    <div @click="onPrev" v-if="hasPrev" class="ScrollControlPrev index-navigation hover-pointer"/>
+    <div @click="onNext" v-if="hasNext" class="ScrollControlNext index-navigation hover-pointer"/>
+
+    <div class="HorizontalScrollView zero-spacing index-content" :class="{container}">
+      <div ref="scrollArea" class="ScrollArea index-content" :style="container ? '' : 'margin: 0 -15px'">
+        <div class="Content" :style="{paddingLeft: `${15-padding/2}px`, paddingRight: `${15-padding/2}px`}">
+          <div class="Item" :style="{paddingLeft: `${padding/2}px`, paddingRight: `${padding/2}px`}"
+               v-for="item in items" :key="mapKey(item)">
+            <slot :item="item">{{item}}</slot>
+          </div>
         </div>
       </div>
     </div>
@@ -30,12 +36,41 @@
         type: Function,
         required: true
       }
+    },
+    data() {
+      return {windowWidth: 0, scrollWidth: 0, distance: 0, offset: 0}
+    },
+    mounted() {
+      this.windowWidth = window.innerWidth <= 1200 ? window.innerWidth : 1200
+      this.distance = this.windowWidth * 0.7 // Distance to cover per nav click
+      this.scrollWidth = this.$refs.scrollArea.scrollWidth
+    },
+    computed: {
+      hasPrev() {
+        return this.offset > 0
+      },
+      hasNext() {
+        return this.windowWidth < this.scrollWidth
+      },
+    },
+    methods: {
+      onPrev() {
+        const scrollArea = this.$refs.scrollArea
+        scrollArea.scrollLeft = scrollArea.scrollLeft - this.distance
+        this.offset = scrollArea.scrollLeft - this.distance
+      },
+      onNext() {
+        const scrollArea = this.$refs.scrollArea
+        scrollArea.scrollLeft = scrollArea.scrollLeft + this.distance
+        this.offset = scrollArea.scrollLeft + this.distance
+      }
     }
   }
 </script>
 
 <style scoped lang="less">
   .HorizontalScrollView {
+    height: 100%;
     overflow-x: visible;
     overflow-y: hidden;
     position: relative;
@@ -50,6 +85,7 @@
       overflow-x: scroll;
       overflow-y: hidden;
       -webkit-overflow-scrolling: touch;
+      scroll-behavior: smooth;
     }
 
     .Content {
@@ -59,6 +95,54 @@
 
       .Item {
         flex-shrink: 0;
+      }
+    }
+  }
+
+  .HorizontalScrollViewWrapper {
+    height: 100%;
+    overflow-y: visible;
+    position: relative;
+
+    .ScrollControlPrev, .ScrollControlNext {
+      @media (max-width: 767.98px) {
+        display: none;
+      }
+
+      position: absolute;
+      height: 100%;
+      width: 48px;
+
+      transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
+
+      &::after {
+        position: absolute;
+        content: "";
+
+        top: calc(50%);
+        margin-top: -6px;
+        height: 14px;
+        width: 14px;
+        border-right: 2px solid black;
+        border-bottom: 2px solid black;
+      }
+    }
+
+    .ScrollControlPrev {
+      left: -25px;
+
+      &::after {
+        left: 20px;
+        transform: rotate(135deg);
+      }
+    }
+
+    .ScrollControlNext {
+      right: -25px;
+
+      &::after {
+        right: 20px;
+        transform: rotate(-45deg);
       }
     }
   }
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -28,7 +28,7 @@
         <p>The best Breakfast spots near you.</p>
       </div>
 
-      <horizontal-scroll-view class="TimingList" :items="[0,1,2,3,4,5,6,7,8,9]" :map-key="i => i">
+      <horizontal-scroll-view class="TimingList" :items="[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]" :map-key="i => i">
         <template slot-scope="{item}">
           <div class="SimpleCard"></div>
         </template>
