3095add686b8e0bd6ba7ee466d9da2527a06286b
Fuxing Loh <lohfuxing@gmail.com>
2018-12-11 22:26:37 +0800

    Location Page Design completed

--- website/components/search/cards/location/SearchCardLocationArea.vue
+++ website/components/search/cards/location/SearchCardLocationArea.vue
@@ -1,18 +1,57 @@
 <template>
-  <div>
-    <h2>Location Area</h2>
+  <div class="mt-24">
+    <div class="flex-between flex-justify-center">
+      <div>
+        <h2>Discover {{area.name}}</h2>
+      </div>
+
+      <div class="none tablet-b desktop-b">
+        <div class="weight-600 s700 hover-pointer" @click="onMore">Show all places in {{area.name}}</div>
+      </div>
+    </div>
+
+    <horizontal-scroll-view class="CardList PlaceList container-remove-gutter mt-24" :items="card.places"
+                            :map-key="a => a.placeId"
+                            :padding="24">
+      <template slot-scope="{item}">
+        <place-card class="CardItem" :place="item"/>
+      </template>
+    </horizontal-scroll-view>
+
+    <div class="none mobile-b mt-24">
+      <button class="secondary-outline w-100" @click="onMore">
+        <span class="text-ellipsis-1l">Show all places in {{area.name}}</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+  import PlaceCard from "../../../places/PlaceCard";
+  import HorizontalScrollView from "../../../core/HorizontalScrollView";
+
   export default {
     name: "SearchCardLocationArea",
+    components: {HorizontalScrollView, PlaceCard},
     props: {
       card: {
         type: Object,
         required: true
       }
     },
+    computed: {
+      area() {
+        return this.card.area
+      }
+    },
+    methods: {
+      onMore() {
+        const area = this.card.area
+
+        this.$store.dispatch('filter/location', {areas: [area], type: 'Where'})
+        this.$store.dispatch('search/start')
+      }
+    }
   }
 </script>
 
--- website/components/search/cards/location/SearchCardLocationBanner.vue
+++ website/components/search/cards/location/SearchCardLocationBanner.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h2>Location Banner</h2>
+    <section class="container-remove-gutter">
+      <div class="container overlay wh-100 flex-align-center inline-flex">
+        <div class="flex-column">
+          <h1 class="white">Discover by neighbourhood</h1>
+          <p class="white">Enter a location and we’ll tell you what’s delicious around.</p>
+          <button class="s700 mt-24">Enter Location</button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -17,5 +25,19 @@
 </script>
 
 <style scoped lang="less">
+  //noinspection CssUnknownTarget
+  section {
+    margin-top: -30px;
 
+    background: url("~/assets/img/search/location-banner.jpg") no-repeat center/cover;;
+    height: 300px;
+  }
+
+  .overlay {
+    background: rgba(0, 0, 0, 0.4);
+  }
+
+  button {
+    align-self: flex-end;
+  }
 </style>
