b55bf99d2d0bd7d9998dc7b1bd6adfb1c874c7f3
Fuxing Loh <fuxing@munch.space>
2019-04-05 15:34:40 +0800

    Updated SearchHomeSeries

--- api/api-search/src/main/java/munch/api/search/plugin/series/SearchHomeSeriesPlugin.java
+++ api/api-search/src/main/java/munch/api/search/plugin/series/SearchHomeSeriesPlugin.java
@@ -22,22 +22,40 @@ public final class SearchHomeSeriesPlugin extends AbstractSearchSeriesListPlugin
 
         List<Position> positions = new ArrayList<>();
 
-        // Award Winning Places
-        load("111f8ec8-50b2-40bf-abd9-95c872ed3ee4", 10).ifPresent(card -> {
-            card.getOptions().put("expand", "height");
-            positions.add(ofPosition(100, card));
+        // 6 iconic hawker centres
+        load("1cc572bd-1f64-49ad-b931-c3248b2f7390", 10).ifPresent(card -> {
+            card.getOptions().put("expand", "width");
+            positions.add(ofPosition(101, card));
+        });
+
+        // Edible Editorials
+        load("61e40e06-2df0-44dd-b375-abb435023efb", 10).ifPresent(card -> {
+            card.getOptions().put("expand", "width");
+            positions.add(ofPosition(102, card));
+        });
+
+        // The Sweetlist
+        load("ef3de46f-12f6-466b-9258-fa4e38faed87", 10).ifPresent(card -> {
+            card.getOptions().put("expand", "width");
+            positions.add(ofPosition(103, card));
         });
 
         // Top 10s
         load("675971ec-2003-46fe-aee9-cb15a692df61", 10).ifPresent(card -> {
             card.getOptions().put("expand", "width");
-            positions.add(ofPosition(101, card));
+            positions.add(ofPosition(104, card));
         });
 
         // The Usual Suspects
         load("e0d039ae-64ea-4713-a999-3e90aadaaca4", 10).ifPresent(card -> {
             card.getOptions().put("expand", "width");
-            positions.add(ofPosition(102, card));
+            positions.add(ofPosition(105, card));
+        });
+
+        // Award Winning Places
+        load("111f8ec8-50b2-40bf-abd9-95c872ed3ee4", 10).ifPresent(card -> {
+            card.getOptions().put("expand", "height");
+            positions.add(ofPosition(106, card));
         });
 
         return positions;
