2958a596072065b5a1aa7af16b68082351b1b89c
Fuxing Loh <fuxing@munch.space>
2019-09-12 13:39:09 +0800

    Changed index.vue to Article focused design

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,16 +1,28 @@
+<template>
+  <div class="container pt-48 pb-128">
+    <div v-for="(node, index) in nodes" :key="index">
+      <home-featured v-if="index === 0" :node="node"/>
+      <home-publication v-else :node="node"/>
+    </div>
+  </div>
+</template>
 <script>
-  import search from './search/index'
+  import HomeFeatured from "../components/home/HomeFeatured";
+  import HomePublication from "../components/home/HomePublication";
 
   export default {
-    extends: search,
+    components: {HomePublication, HomeFeatured},
     head() {
       return this.$head({
         robots: {follow: true, index: true},
         url: `https://www.munch.app/`,
       })
     },
-    asyncData({store}) {
-      return store.dispatch('search/feature', {feature: 'Home'})
+    asyncData({$api}) {
+      return $api.get('/pages/home')
+        .then(({data: nodes}) => {
+          return {nodes}
+        })
     },
   }
 </script>
