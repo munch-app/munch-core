1466a85e50f432faf984b66fa056cd122c94ad2e
Fuxing Loh <fuxing@munch.space>
2019-09-20 11:49:31 +0800

    Added partner page, WIP

---
+++ website/pages/partner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container pt-48 pb-128">
+    <div>
+        <!-- TODO(fuxing): WIP -->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="less">
+
+</style>
