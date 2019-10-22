74f5b8ba5510eec198d6e5e04ad9d9c064abc7be
Fuxing Loh <lohfuxing@gmail.com>
2019-10-22 17:51:24 +0800

    Completed and deployed profile media worker

--- worker/worker-profile-media/src/main/java/app/munch/worker/MediaWorker.java
+++ worker/worker-profile-media/src/main/java/app/munch/worker/MediaWorker.java
@@ -2,13 +2,18 @@ package app.munch.worker;
 
 import app.munch.database.DatabaseModule;
 import app.munch.image.ImageModule;
+import app.munch.model.ProfileMedia;
+import app.munch.model.ProfileMediaStatus;
 import app.munch.model.WorkerTask;
+import dev.fuxing.jpa.HibernateUtils;
 import dev.fuxing.jpa.TransactionProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author Fuxing Loh
@@ -19,10 +24,12 @@ public final class MediaWorker implements WorkerRunner {
     private static final Logger logger = LoggerFactory.getLogger(MediaWorker.class);
 
     private final TransactionProvider provider;
+    private final MediaAnalyser analyser;
 
     @Inject
-    MediaWorker(TransactionProvider provider) {
+    MediaWorker(TransactionProvider provider, MediaAnalyser analyser) {
         this.provider = provider;
+        this.analyser = analyser;
     }
 
     @Override
@@ -31,8 +38,26 @@ public final class MediaWorker implements WorkerRunner {
     }
 
     @Override
-    public void run(WorkerTask task) {
+    public void run(WorkerTask task) throws IOException {
+        do {
+            List<ProfileMedia> medias = provider.reduce(true, entityManager -> {
+                List<ProfileMedia> list = entityManager.createQuery(
+                        "FROM ProfileMedia WHERE status = :status ORDER BY createdAt DESC",
+                        ProfileMedia.class)
+                        .setParameter("status", ProfileMediaStatus.PENDING)
+                        .setMaxResults(10)
+                        .getResultList();
 
+                list.forEach(media -> {
+                    HibernateUtils.initialize(media.getImages());
+                });
+                return list;
+            });
+
+            for (ProfileMedia media : medias) {
+                analyser.analyse(media);
+            }
+        } while (true);
     }
 
     public static void main(String[] args) {
