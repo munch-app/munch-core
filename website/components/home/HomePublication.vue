70b860cef60dcfd7152c6d9900c5cde33c29b62c
Fuxing Loh <lohfuxing@gmail.com>
2019-10-23 18:02:23 +0800

    Fixed HomePublication responsive layout issue

--- website/components/home/HomePublication.vue
+++ website/components/home/HomePublication.vue
@@ -4,10 +4,10 @@
       <h1>{{publication.name}}</h1>
     </div>
 
-    <div class="Content flex-wrap">
-      <section class="Main">
-        <div class="flex-1-2">
-          <div class="Article" v-for="article in mainArticles" :key="article.id">
+    <div class="m--12 flex-wrap">
+      <section class="p-12 Main">
+        <div class="m--12 flex-1-2">
+          <div class="p-12 Article" v-for="article in mainArticles" :key="article.id">
             <nuxt-link class="text-decoration-none" :to="`/@${article.profile.username}/${article.slug}-${article.id}`">
               <div class="aspect r-5-3 border-2 overflow-hidden flex-no-shrink mr-16">
                 <cdn-img :image="article.image" type="640x640">
@@ -23,9 +23,9 @@
         </div>
       </section>
 
-      <section class="Side">
-        <div>
-          <div class="Article" v-for="article in sideArticles" :key="article.id">
+      <section class="p-12 Side">
+        <div class="m--8">
+          <div class="p-8 Article" v-for="article in sideArticles" :key="article.id">
             <nuxt-link class="flex-row text-decoration-none"
                        :to="`/@${article.profile.username}/${article.slug}-${article.id}`">
               <div class="wh-80px border-2 overflow-hidden flex-no-shrink mr-16">
@@ -73,43 +73,23 @@
 </script>
 
 <style scoped lang="less">
-  .Content {
-    margin: -12px;
-  }
-
   .Main {
     flex: 0 0 65%;
     max-width: 65%;
-    padding: 12px 24px 12px 12px;
-
-    > div {
-      margin: -12px;
-    }
-
-    .Article {
-      padding: 12px;
-    }
+    padding-right: 24px;
   }
 
   .Side {
     flex: 0 0 35%;
     max-width: 35%;
-    padding: 12px;
-
-    > div {
-      margin: -8px;
-    }
-
-    .Article {
-      padding: 8px;
-    }
   }
 
   @media (max-width: 767.98px) {
     .Main {
-      flex: 0 0 calc(100% + 24px);
-      max-width: calc(100% + 24px);
-      padding: 12px;
+      flex: 0 0 100%;
+      max-width: 100%;
+
+      padding-right: 12px;
     }
 
     .Side {
